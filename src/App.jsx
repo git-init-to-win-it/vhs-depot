9df747864f9963fb34ea193ee1966a2e4d212129
Thomas <thomasghunter@gmail.com>
2024-05-01 08:43:18 -0700

    fixed route path names

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <Route path="/movies/:id" element={<h1>DETAILS PAGE</h1>}/>
         <Route path="/login" element={<h1>LOGIN PAGE</h1>}/>
         <Route path="/register" element={<h1>REGISTER PAGE</h1>}/>
-        <Route path="/me" element={<h1>ACCOUNT PAGE/CART</h1>}/>
+        <Route path="/cart" element={<h1>CART</h1>}/>
     </Routes>
     </>
   )
