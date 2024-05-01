afbb28dd7db7059d050328ce3246efa7d11dde48
Kelsey Cordero <kcordero99@gmail.com>
2024-05-01 12:14:46 -0400

    modified changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from "react-router-dom";
-import NavBar from './components/NavBar';
+
 function App() {
 
   return (
     <>
-    <NavBar/>
       <Routes>
         <Route path="/" element={<h1>HOME</h1>} />
         <Route path="/movies/:id" element={<h1>DETAILS PAGE</h1>} />
