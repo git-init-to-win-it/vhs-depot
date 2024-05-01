e9ee10ec136bd860c897e7b1181632c0987803e8
Thomas <thomasghunter@gmail.com>
2024-05-01 08:41:21 -0700

    fixed route paths

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
     <>
     <Routes>
         <Route path="/" element={<h1>HOME</h1>}/>
-        <Route path="/:id" element={<h1>DETAILS PAGE</h1>}/>
-        <Route path="/auth/login" element={<h1>LOGIN PAGE</h1>}/>
-        <Route path="/auth/register" element={<h1>REGISTER PAGE</h1>}/>
+        <Route path="/movies/:id" element={<h1>DETAILS PAGE</h1>}/>
+        <Route path="/login" element={<h1>LOGIN PAGE</h1>}/>
+        <Route path="/register" element={<h1>REGISTER PAGE</h1>}/>
         <Route path="/me" element={<h1>ACCOUNT PAGE/CART</h1>}/>
     </Routes>
     </>
