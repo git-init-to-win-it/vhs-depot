19a45f789f4f66258c6125907de4f37b18dd218d
Kelsey Cordero <kcordero99@gmail.com>
2024-05-01 10:58:40 -0400

    added a note on navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 
 const NavBar = ({token, setToken}) => {
   //created ternary for logged in and non-logged in users. They can be identified by token.
+  //users are redirected to homepage once logged out.
   return (
     <>
       <Link to="/">Home</Link>
