f7c37e4bb1de42ce3aadc0ab99648ef8ec915f69
Thomas <thomasghunter@gmail.com>
2024-05-01 13:49:16 -0700

    changed script and commented out client file

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
-const { Client } = require('pg');
-const client = new Client(process.env.DATABASE_URL || "postgresql://localhost:5432/vhsdepot")
+// require("dotenv").config();
+// const { Client } = require('pg');
+// const client = new Client(process.env.DATABASE_URL || "postgresql://localhost:5432/vhsdepot")
 
-module.exports = client
+// module.exports = client
