61501b823db4109f9a43b923111dcaffcc4bab5b
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-05-01 14:14:23 -0500

    port render changes

--- server.cjs
+++ server.cjs
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 const apiRouter = require("./api/index.cjs")
 
 //Middleware
