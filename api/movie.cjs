140679db7c3f9b2aa1e3921f7edea8182c4277a8
Thomas <thomasghunter@gmail.com>
2024-05-02 11:26:54 -0700

    21 get all movies ticket

--- api/movie.cjs
+++ api/movie.cjs
@@ -1,8 +1,18 @@
+const {getAllMovies} = require('../db')
 const express = require("express")
 const movieRouter = express.Router()
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
 
 movieRouter.get("/", async (req, res, next) => {
-  res.send("This is the movie routes api ")
+  try {
+    const allMovies = await getAllMovies()
+    res.send(allMovies);
+  } catch (error) {
+    console.log(error)
+    
+  }
 })
 
 module.exports = movieRouter
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import GetAllMovies from './components/getAllMovies';
+const API_URL = "postgres://vhsdepot_user:S6IJK67rtCIGZ5hSNilHGcWcxOkzPms2@dpg-cop777acn0vc73dnnrm0-a.oregon-postgres.render.com/vhsdepot"
+
 
 function App() {
   const [token, setToken] = useState("")
@@ -14,7 +17,7 @@ function App() {
     <>
       <NavBar token={token} setToken={setToken}/>
       <Routes>
-        <Route path="/" element={<h1>HOME</h1>} />
+        <Route path="/" element={<GetAllMovies API_URL={API_URL}/>} />
         <Route path="/movies/:id" element={<h1>DETAILS PAGE</h1>} />
         <Route path="/login" element={<h1>LOGIN PAGE</h1>} />
         <Route path="/register" element={<h1>REGISTER PAGE</h1>} />
