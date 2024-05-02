554abb3f1032d9ffbbeab8ad04dec0dedb745542
Kevin Browning <kbrowning1996@gmail.com>
2024-05-02 12:15:24 -0400

    changed footer to just back to top button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,6 @@ import BackToTopButton from './BackToTopButton';
 const Footer = ({token, setToken}) => {
   return (
     <>
-      <Link to="/">Home</Link>
-      {token ? (
-        <> 
-        <Link to="/cart">Cart</Link>
-        <Link onClick={(e) => {
-          setToken("");
-          localStorage.setItem("token", "");
-        }}
-        to="/"
-        >
-          Logout
-        </Link>
-      </>
-      ) : (
-        <Link to="/login">Login</Link>
-      )}
       <BackToTopButton />
     </>
   );
