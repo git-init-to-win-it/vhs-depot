59aca4f14735edcd7131cc6dd2df731dfe287897
Kelsey Cordero <kcordero99@gmail.com>
2024-05-03 14:16:15 -0400

    rendered form on login screen, now functional

--- src/pages/authPages/Login.jsx
+++ src/pages/authPages/Login.jsx
@@ -1,13 +1,66 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return (
-  <>
-  <h1>LOGIN</h1>
-  <Link to="/register">REGISTER</Link>
+const Login = ({ setToken }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
   
-  </>
+
+  const submitHandler = async(e) => {
+    e.preventDefault();
+    try{
+      const result = await fetch("/auth/login", {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+          username,
+          password
+        })
+      });
+      console.log(result);
+      const json = await result.json();
+      //console.log(json);
+
+      if(json.token){
+        localStorage.setItem('token', json.token);
+        setToken(json.token);
+        navigate("/");
+      }
+    }catch(error){
+      console.log("ERROR CAUGHT WHEN FETCHING API");
+    }
+  } 
+
+  return (
+    <>
+      <h3>LOGIN</h3>
+      <form onSubmit={submitHandler}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => { setUsername(e.target.value) }} />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => { setPassword(e.target.value) }} />
+        </label>
+        <input type="submit" value="Log in" />
+      </form>
+      <label>
+        Don't have an account?
+        <Link to="/register">Sign up</Link>
+      </label>
+
+    </>
   )
 }
 
