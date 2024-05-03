90d9d043a00e99da6f4daf63963a01af5956949f
Kevin Browning <kbrowning1996@gmail.com>
2024-05-03 16:03:12 -0400

    added cart button

---
+++ src/components/CartButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+  const CartButton = () => {
+    return (
+    <button
+      className="cartButton"
+      onClick={<Link to="/cart"></Link>}
+      style={{ display: 'block' }}
+    >
+      Cart
+    </button>
+  );
+};
+
+export default CartButton;
