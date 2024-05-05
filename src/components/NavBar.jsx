faa09b60e54f36e2c4d204f197c26e5fa72db6ea
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-05-05 17:40:57 -0500

    finished super basic navbar for better dev testing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,43 @@
-import { Link } from 'react-router-dom'
-import CartButton from './CartButton';
+import { Link } from "react-router-dom"
+import CartButton from "./CartButton"
+import "../styles/navbar.css"
 
-const NavBar = ({token, setToken}) => {
+const NavBar = ({ token, setToken }) => {
   //created ternary for logged in and non-logged in users. They can be identified by token.
   //users are redirected to homepage once logged out.
   return (
     <>
-      <Link to="/">Home</Link>
-      {token ? (
-        <> 
-        <Link to="/cart"><CartButton /></Link>
-        <Link onClick={(e) => {
-          setToken("");
-          localStorage.setItem("token", "");
-        }}
-        to="/"
-        >
-          Logout
-        </Link>
-      </>
-      ) : (
-        <Link to="/login">Login</Link>
-      )}
+      <ul className="navbar-ul">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {token ? (
+          <>
+            <li>
+              <Link to="/cart">
+                <CartButton />
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={e => {
+                  setToken("")
+                  localStorage.setItem("token", "")
+                }}
+                to="/"
+              >
+                Logout
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+      </ul>
     </>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
