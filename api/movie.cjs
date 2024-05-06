c180ec79c24d0c345d0c4a68fba6e3a87ab65889
Kevin Browning <kbrowning1996@gmail.com>
2024-05-06 14:24:26 -0400

    added api routes in movies.cjs for put request to add and remove movies from cart

--- api/movie.cjs
+++ api/movie.cjs
@@ -132,10 +132,102 @@ movieRouter.put("/:id", async (req, res, next) => {
       })
       return res.status(201).json(updatedMovie)
     }
+    next();
   } catch (err) {
     console.error(err)
     return res.status(401).send(`You do not have authorization for this`)
   }
-})
+});
+
+movieRouter.put("/add/:id", async (req, res, next) => {
+  const id = parseInt(req.params.id)
+  const authHeader = req.headers["authorization"]
+  if (!authHeader) {
+    return res.status(401).send(`Please try logging in first`)
+  }
+  const prefix = `Bearer `
+  const token = authHeader.slice(prefix.length)
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userid = decoded.id;
+    const verifyUser = await prisma.users.findUnique({
+      where: {
+        id: userid
+      },
+      select: {
+        cart: true
+      }
+    });
+
+    if (verifyUser) {
+      const movie = await prisma.movies.findUnique({
+        where: {
+          id: id
+        }
+      });
+
+      if (movie) {
+        if (movie.cartid === null) {
+          const updatedMovie = await prisma.movies.update({
+            where: {
+              id: id
+            },
+            data: {
+              cartid: verifyUser.cart[0].id
+            },
+          });
+          return res.status(201).json(updatedMovie);
+        } else {
+          return res.status(400).send("Movie is out of stock, please try again later!");
+        }
+      } else {
+        return res.status(404).send("Movie not found.");
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(401).send(`You do not have authorization for this`);
+  }
+});
+
+movieRouter.put("/remove/:id", async (req, res, next) => {
+  const id = parseInt(req.params.id)
+  const authHeader = req.headers["authorization"]
+  if (!authHeader) {
+    return res.status(401).send(`Please try logging in first`)
+  }
+  const prefix = `Bearer `
+  const token = authHeader.slice(prefix.length)
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const userid = decoded.id
+    const verifyUser = await prisma.users.findUnique({
+      where: {
+        id: userid
+      },
+      select: {
+        cart: true
+      }
+    })
+
+    if (verifyUser) {
+      const updatedMovie = await prisma.movies.update({
+        where: {
+          id: id,
+        },
+        data: {
+          cartid: null
+        },
+      })
+      return res.status(201).json(updatedMovie)
+    }
+    next();
+  } catch (err) {
+    console.error(err)
+    return res.status(401).send(`You do not have authorization for this`)
+  }
+});
 
 module.exports = movieRouter
